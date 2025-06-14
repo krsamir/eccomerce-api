102c8e9a63b79eb65e8738b5fb53d78d0b4bb737
krsamir <samir1741012321@gmail.com>
2025-06-14 19:35:16 +0530

    removed conflicting package

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import { coorelation, logger, ENVIRONMENT } from "@ecom/utils";
 import cors from "cors";
 import { CONSTANTS } from "@ecom/utils";
 import httpErrors from "http-errors";
-import "express-async-errors";
 
 const app = express();
 
@@ -26,7 +25,7 @@ app.use((req, res, next) => {
   next(httpErrors(404));
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   logger.error(`GLOBAL ERROR HANDLER::\n ${_.inspect(err, { depth: 6 })}`);
 
   const error =
