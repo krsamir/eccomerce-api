af34eb5150f377422afe154ed8a080fbbfc880e4
krsamir <samir1741012321@gmail.com>
2025-06-21 23:42:22 +0530

    changed eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default defineConfig([
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "warn",
+      "no-async-promise-executor": "off",
     },
   },
   eslintConfigPrettier,
